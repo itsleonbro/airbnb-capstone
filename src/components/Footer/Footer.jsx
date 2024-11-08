b85ef4276ccb05f2edc36401adf356ad975f6c0f
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-11-08 23:48:28 +0200

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.footer}>
+      <div className={styles.footerContainer}>
+        <div className={styles.support}>
+          <h2>Support</h2>
+          <ul>
+            <li>Help Center</li>
+            <li>Safety information</li>
+            <li>Cancellation options</li>
+            <li>Our COVID-19 Response</li>
+            <li>Supporting people with disabilities</li>
+            <li>Report a neighborhood concern</li>
+          </ul>
+        </div>
 
-export default Footer
+        <div className={styles.community}>
+          <h2>Community</h2>
+          <ul>
+            <li>Airbnb.org: disaster relief housing</li>
+            <li>Support: Afghan refugees</li>
+            <li>Celebrating diversity & belonging</li>
+            <li>Combating discrimination</li>
+          </ul>
+        </div>
+
+        <div className={styles.hosting}>
+          <h2>Hosting</h2>
+          <ul>
+            <li>Hosting</li>
+            <li>Try hosting</li>
+            <li>AirCover: protection for Hosts</li>
+            <li>Explore hosting resources</li>
+            <li>Visit our community forum</li>
+            <li>How to host responsibly</li>
+          </ul>
+        </div>
+
+        <div className={styles.about}>
+          <h2>About</h2>
+          <ul>
+            <li>About</li>
+            <li>Newsroom</li>
+            <li>Learn about new features</li>
+            <li>Letter from our founders</li>
+            <li>Careers</li>
+            <li>Investors</li>
+            <li>Airbnb Luxe</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
