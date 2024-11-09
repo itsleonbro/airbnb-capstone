ac905785a00a55c1685c9126fb9b4b1ddf275809
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-11-09 03:34:55 +0200

    conditionally render footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import NotFound from "./pages/NotFound/NotFound";
 
 const App = () => {
+  const location = useLocation();
+
+  // paths where footer wont be rendered
+  const noFooterPaths = ["/login"];
+
   return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+
+      {/* conditionally rendered footer*/}
+      {!noFooterPaths.includes(location.pathname) && <Footer />}
+    </div>
   );
 };
 
