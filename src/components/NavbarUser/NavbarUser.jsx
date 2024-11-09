caffa708383cfa3bd4f2b0b39caba0f35e4eaef3
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-11-09 20:59:49 +0200

    add navbar with user details

---
+++ src/components/NavbarUser/NavbarUser.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./NavbarUser.module.css";
+import AvatarMenu from "../AvatarMenu/AvatarMenu";
+
+const NavbarUser = () => {
+  return (
+    <nav>
+      <div className={styles.nav}>
+        <div className={styles.navContainer}>
+          <Link to="/home">
+            <img src="../src/assets/airbnb.svg" alt="logo" height={32} />
+          </Link>
+
+          <div className={styles.rightMenu}>
+            <p>Welcome, Leon</p>
+            <AvatarMenu />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarUser;
