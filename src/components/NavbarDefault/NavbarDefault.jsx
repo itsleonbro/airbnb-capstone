68ad48c291bbf1e00d6f04459ee157a009769158
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-11-09 20:59:08 +0200

    add avatar menu component to default navbar

--- src/components/NavbarDefault/NavbarDefault.jsx
+++ src/components/NavbarDefault/NavbarDefault.jsx
@@ -1,37 +1,50 @@
-import React from "react";
-import "./NavbarDefault.css";
+import React, { useState } from "react";
+import styles from "./NavbarDefault.module.css"; // Import styles as a module
+import ProfilePopUp from "../ProfilePopUp/ProfilePopUp";
 import { Link } from "react-router-dom";
-import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
-import PermIdentityRoundedIcon from "@mui/icons-material/PermIdentityRounded";
+import AvatarMenu from "../AvatarMenu/AvatarMenu";
 
 const NavbarDefault = () => {
+  const [profilePopUp, isProfilePopUp] = useState(false);
+
+  const handleProfileClick = () => {
+    isProfilePopUp(!profilePopUp);
+  };
+
   return (
     <>
       <nav>
-        <div className="logo">
-          <Link to="/home">
-            <img src="./src/assets/airbnb-white.svg" alt="airbnb logo" height={32} />
-          </Link>
-        </div>
-        <div className="center-menu">
-          <ul>
-            <li>Places to stay</li>
-            <li>Experiences</li>
-            <li>Online Experiences</li>
-          </ul>
-        </div>
-        <div className="right-menu">
-          <ul>
-            <li>Become a Host</li>
-            <li className="profile">
-              <span>
-                <MenuRoundedIcon sx={{ color: "#000" }} />
-              </span>
-              <span>
-                <PermIdentityRoundedIcon sx={{ color: "#000" }} />
-              </span>
-            </li>
-          </ul>
+        <div className={styles.navContainer}>
+          <div className={styles.logo}>
+            <Link to="/home">
+              <img
+                src="./src/assets/airbnb-white.svg"
+                alt="airbnb logo"
+                height={32}
+              />
+            </Link>
+          </div>
+          <div className={styles.centerMenu}>
+            <ul>
+              <li>Places to stay</li>
+              <li>Experiences</li>
+              <li>Online Experiences</li>
+            </ul>
+          </div>
+
+          <div className={styles.rightMenu}>
+            <ul>
+              <Link to="/login">
+                <li>Become a Host</li>
+              </Link>
+
+              <li className={styles.profile} onClick={handleProfileClick}>
+                <AvatarMenu />
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.popup}>{profilePopUp && <ProfilePopUp />}</div>
         </div>
       </nav>
     </>
