69108eae428d180f648e4c1e568e7a177da29553
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-11-20 23:21:39 +0200

    add datepicker to searchbar and guest popup

--- src/components/SearchFilter/SearchFilter.jsx
+++ src/components/SearchFilter/SearchFilter.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import styles from "./SearchFilter.module.css";
-import { colors } from "@mui/material";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import GuestPopup from "../GuestPopup/GuestPopup";
 
 const SearchFilter = () => {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [guestCount, setGuestCount] = useState(1);
+  const [isGuestPopupOpen, setIsGuestPopupOpen] = useState(false);
+
+  console.log(isGuestPopupOpen);
+
   return (
     <div className={styles.searchbar}>
       <div className={styles.searchbarContainer}>
         <div className={styles.hotels}>
-          <label htmlFor="hotels">Hotels</label>
-          <div className={styles.dropdown}>Select location</div>
+          <label htmlFor="hotels" className={styles.title}>
+            Locations
+          </label>
+
           <select name="" id="">
+            <option value="hotel one">Select location</option>
             <option value="hotel one">hotel one</option>
             <option value="hotel two">hotel two</option>
           </select>
@@ -18,17 +30,46 @@ const SearchFilter = () => {
 
         <div className={styles.checkIn}>
           <div className={styles.title}>Check in</div>
-          <div className={styles.dropdown}>Add dates</div>
+
+          <div className={styles.dropdown}>
+            <DatePicker
+              className={styles.datePicker}
+              placeholderText="Add dates"
+              selected={startDate}
+              onChange={date => setStartDate(date)}
+              dateFormat="dd/MM/yyyy"
+            />
+          </div>
         </div>
 
         <div className={styles.checkOut}>
           <div className={styles.title}>Check out</div>
-          <div className={styles.dropdown}>Add dates</div>
+          <DatePicker
+            className={styles.datePicker}
+            placeholderText="Add dates"
+            selected={endDate}
+            onChange={date => setEndDate(date)}
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
 
-        <div className={styles.guests}>
-          <div className={styles.title}>Guest</div>
+        <div
+          className={styles.guests}
+          onClick={() => setIsGuestPopupOpen(!isGuestPopupOpen)}
+        >
+          <div className={styles.title}>Guests</div>
           <div className={styles.dropdown}>Add guests</div>
+          {isGuestPopupOpen && (
+            <div
+              className={styles.guestPopup}
+              onClick={e => e.stopPropagation()}
+            >
+              <GuestPopup
+                setGuestCount={setGuestCount}
+                guestCount={guestCount}
+              />
+            </div>
+          )}
         </div>
 
         <div className={styles.search}>
