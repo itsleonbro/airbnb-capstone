3b0224519d4f11d59a951bc05d4915ed12d78141
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2025-03-19 19:21:53 +0200

    refactor inspiration cards layout

--- src/components/InspirationCard/InspirationCard.jsx
+++ src/components/InspirationCard/InspirationCard.jsx
@@ -14,20 +14,3 @@ const InspirationCard = ({ title, distance }) => {
 };
 
 export default InspirationCard;
-
-// import React from "react";
-// import styles from "./InspirationCard.module.css";
-
-// const InspirationCard = () => {
-//   return (
-//     <div className={styles.cardContainer}>
-//       <img src="/assets/inspirationCard/sandtoncity.jpeg" alt="" />
-//       <div className={styles.cardInfo}>
-//         <h2> Sandton City Hotel</h2>
-//         <p>53km away</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default InspirationCard;
